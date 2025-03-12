04332108b8097e70bdff937916620677e96f9237
hyuk <k941026h@naver.com>
2025-03-12 23:27:00 +0900

    Project start

---
+++ src/main/java/com/bb/webdrawerservice/WebdrawerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bb.webdrawerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdrawerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdrawerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bb/webdrawerservice/WebdrawerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bb.webdrawerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdrawerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
